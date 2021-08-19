0d2f7f8d29e70792a0d5ab795be3fc4ca416cdc6
Gerald Heng <geralddevelop@gmail.com>
2021-08-19 11:11:12 +0800

    added mqtt server to ui

--- dt_client.py
+++ dt_client.py
@@ -24,10 +24,11 @@ logs = []
 
 # Count to 79 as each subscribe interval will have 79 message.
 line_counter = 0
+cycle = 0
 temp_variable_dict = {}
+serverClient = None
 # current_source = 'mqtt'
 current_source = 'log'
-current_source = 'loasdg'
 
 
 class ServerClient:
@@ -47,7 +48,7 @@ class ServerClient:
         print('connecting')
         self.server.connect(self.broker_address, port=self.port)
         self.server.loop_start()
-        time.sleep(2)
+        time.sleep(1)
         self.server.subscribe(self.topic)
 
     def sort_by_origin(self):
@@ -75,9 +76,10 @@ def on_connect(client, userdata, flags, rc):
 
 # When message read from server, this is where it will go. Use for dt mode.
 def on_message(client, userdata, message):
-    # print(message.payload.decode("utf-8"))
     global line_counter
     global temp_variable_dict
+    global serverClient
+    global cycle
     dt_item = json.loads(message.payload.decode("utf-8"))[0]
 
     # Get the name of the component.
@@ -98,12 +100,8 @@ def on_message(client, userdata, message):
     value = float(dt_item['value'][dt_item['value'].find(
         ':')+1: len(dt_item['value'])].strip())
 
-    print('component', component)
-    print('element', element)
-    print('value', value)
 
     line_counter += 1
-    print('line counter', line_counter)
 
     if component in temp_variable_dict:
         temp_variable_dict[component][element] = value
@@ -112,96 +110,21 @@ def on_message(client, userdata, message):
 
     if line_counter >= 79:
         line_counter = 0
-        print('finish message interval')
-        print(temp_variable_dict)
+        cycle += 1
+        for component, component_val in DT_VALUES.items():
+            for item, item_val in component_val.items():
+                if item_val['type'] is 'switch':
+                    Switch.define_switch_dt(
+                        item, temp_variable_dict[item], serverClient.variables, component)
+                elif item_val['type'] is 'ied':
+                    Ied.define_ied_dt(
+                        item, temp_variable_dict[item], serverClient.variables, component)
 
-        for name, temp_var in temp_variable_dict.items():
-            print(name)
-            print(temp_var)
-            if name[0] is 'Q':
-                # Switch.define_switch_dt(name, val, variable_dict)
-                print(name, 'is a switch')
-            elif 'meter' in name.lower():
-                # Ied.define_ied_dt(
-                #     name, temp_variable_dict[name], variable_dict)
-                print(name, 'is a ied')
-            else:
-                print(name, 'is G1, G2 or motor')
-
-    # if component in temp_variable_dict:
-    #     temp_variable_dict[component][element] = value
-    # else:
-    #     temp_variable_dict[component] = {element: value}
-
-    # if name[0] is 'Q':
-    #     Switch.define_switch_dt(name, val, variable_dict)
-    #     print(name, 'is a switch')
-    # elif 'meter' in name.lower():
-    #     Ied.define_ied_dt(
-    #         name, temp_variable_dict[name], variable_dict)
-    #     print(name, 'is a ied')
-    # else:
-    #     print(name, 'is G1, G2 or motor')
-
-
-def main():
-    # TODO: This is the MQTT to server.
-    # stval/#
-    # serverClient = ServerClient(
-    #     '139.59.97.171', 1883, 'measures/#', on_connect, on_message)
-    # serverClient.connect()
-
-    # # Variable to be read in onMessage
-
-    # try:
-    #     while True:
-    #         time.sleep(1)
-
-    # except KeyboardInterrupt:
-    #     serverClient.server.disconnect()
-    # serverClient.server.loop_stop()
-
-    serverClient = ServerClient()
-    f = open(DATA + 'mqttlog.txt', 'r')
-    line_counter = 0
-    for line in f.readlines():
-
-        dt_item = json.loads(line)[0]
-
-        # Get the name of the component.
-        name = dt_item['value'][(dt_item['value'].find(
-            ']') + 1): dt_item['value'].find(':')].strip()
-
-        # To find the component and element split point.
-        c_index = len(name) - 1
-        for c in name[::-1]:
-            if c == '_':
-                break
-            c_index -= 1
-
-        component = name[0: c_index]
-        element = name[c_index + 1: len(name)]
-
-        # Get the value of the component.
-        value = float(dt_item['value'][dt_item['value'].find(
-            ':')+1: len(dt_item['value'])].strip())
-
-        if component in temp_variable_dict:
-            temp_variable_dict[component][element] = value
-        else:
-            temp_variable_dict[component] = {element: value}
-
-        line_counter += 1
-
-        # Each MQTT call give us 79 line of logs, each 79 lines represents a second/round in dt.
-        if line_counter >= 79:
-            print()
-            line_counter = 0
-
-            time.sleep(1)
-
-    print(temp_variable_dict)
-    print('value is ', json.loads(dt_item['value']))
+        with app.app_context():
+            turbo.push(turbo.replace(render_template(
+                'components.html', origins=serverClient.sort_by_origin()), 'load-components'))
+            turbo.push(turbo.replace(render_template(
+                'timestamp.html', timestamp=f'Cycle: {str(cycle)}'), 'load-timestamp'))
 
 
 @app.route("/")
@@ -215,11 +138,12 @@ def before_first_request():
 
 
 def update_load():
+    global serverClient
     with app.app_context():
         # --------------------- SERVER --------------------------
         if current_source == 'mqtt':
             serverClient = ServerClient(
-              '139.59.97.171', 1883, 'measures/#', on_connect, on_message)
+                '139.59.97.171', 1883, 'measures/#', on_connect, on_message)
             serverClient.connect()
 
             # Variable to be read in onMessage
@@ -232,12 +156,12 @@ def update_load():
                 serverClient.server.loop_stop()
         # --------------------- SERVER END --------------------------
 
-
         # --------------------- TEXT LOGS --------------------------
-        elif current_source == 'log':        
+        elif current_source == 'log':
             serverClient = ServerClient()
             f = open(DATA + 'mqttlog.txt', 'r')
             line_counter = 0
+            cycle = 0
             for line in f.readlines():
 
                 dt_item = json.loads(line)[0]
@@ -269,27 +193,21 @@ def update_load():
 
                 # Each MQTT call give us 79 line of logs, each 79 lines represents a second/round in dt.
                 if line_counter >= 79:
-                    print()
                     line_counter = 0
+                    cycle += 1
                     for component, component_val in DT_VALUES.items():
                         for item, item_val in component_val.items():
                             if item_val['type'] is 'switch':
-                                # if 'connected_to' in item_val:
-                                #     connected_to = item_val['connected_to']
-                                # else:
-                                #     connected_to = None
                                 Switch.define_switch_dt(
                                     item, temp_variable_dict[item], serverClient.variables, component)
-                                print(item, 'is a switch')
                             elif item_val['type'] is 'ied':
                                 Ied.define_ied_dt(
                                     item, temp_variable_dict[item], serverClient.variables, component)
-                                print(item, 'is a ied')
 
                     turbo.push(turbo.replace(render_template(
                         'components.html', origins=serverClient.sort_by_origin()), 'load-components'))
                     turbo.push(turbo.replace(render_template(
-                        'timestamp.html', timestamp=datetime.now().strftime("%H:%M:%S")), 'load-timestamp'))
+                        'timestamp.html', timestamp=f'Cycle: {str(cycle)}'), 'load-timestamp'))
 
                     time.sleep(1)
             # --------------------- TEXT LOGS END --------------------------
